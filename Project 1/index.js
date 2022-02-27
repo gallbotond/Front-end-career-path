70fe1dadbc33bcc1bc7ee419335048ef3adaa609
Gáll Botond <gallbotondofficial@gmail.com>
2022-02-27 13:55:49 +0200

    finished staticly the first project

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,1 +1,28 @@
-document.getElementById('number').innerText = 5
+const counterElement = document.getElementById('number')
+const savedValues = document.getElementById('prev-values')
+
+let count = 0
+let first = true
+savedValues.textContent = 'none'
+
+function increase() {
+    count += 1
+    counterElement.textContent = count
+}
+
+function save() {
+    if (first) savedValues.textContent = ''
+    savedValues.textContent += (!first ? ' - ' : '') + count
+    count = 0
+    first = false
+}
+
+function reset() {
+    counterElement.textContent = '0'
+    count = 0
+}
+
+function del() {
+    savedValues.textContent = 'none'
+    first = true
+}
