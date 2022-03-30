23de0f2f6c198a44cd1f455cd761a91eaac02bd0
Gáll Botond <gallbotondofficial@gmail.com>
2022-03-30 22:34:35 +0300

    created a sketch of second project

---
+++ API/BlogSpace/index.js
@@ -0,0 +1,15 @@
+const main = document.getElementById('posts')
+
+fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+    .then(res => res.json())
+    .then(data => {
+        let html = ''
+        data.slice(0, 5).map(post => html += `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            <hr>
+        `)
+        main.innerHTML = html
+        console.log(main)
+    })
+
