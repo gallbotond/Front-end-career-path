af3662238e87c8784aa7eabd13b07351ff1a3fe7
Gáll Botond <gallbotondofficial@gmail.com>
2022-03-30 21:36:19 +0300

    implemented data fetching and dynamic color change

--- API/index.js
+++ API/index.js
@@ -1,5 +1,35 @@
-const img = document.querySelector('.image-container')
+const activity = document.querySelector('.activity')
+const button = document.getElementById('btn')
+const body = document.body
+const main = document.getElementById('container')
 
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(data => img.innerHTML = `<img src='${data.message}' alt='dog' />`);
+fetch('https://apis.scrimba.com/bored/api/activity')
+  .then(res => res.json())
+  .then(data => {
+    activity.textContent = data.activity
+  })
+
+function randomColors() {
+  let val = Math.floor(Math.random() * 355)
+  let color = `hsl(${val}, 100%, `
+  let lighter = color + '80%)'
+  color += '50%)'
+
+  return [color, lighter, val >= 30 && val <= 70]
+}
+
+button.addEventListener('click', function() {
+  let [color, lighter, dark] = randomColors()
+
+  fetch('https://apis.scrimba.com/bored/api/activity')
+  .then(res => res.json())
+  .then(data => {
+    activity.textContent = data.activity
+
+    body.style.background = lighter
+    activity.style.color = color
+    button.style.color = !dark ? 'white' : 'black'
+    button.style.background = !dark ? color : lighter
+    main.style.background = !dark ? 'white' : 'rgb(120, 120, 120)'
+  })
+})
