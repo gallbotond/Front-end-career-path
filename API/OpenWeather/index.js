0c7d61b8dcd2532ffa4bc8ab6c1f75951840a2d6
Gáll Botond <gallbotondofficial@gmail.com>
2022-04-11 07:29:05 +0300

    added location, time, and weather data

--- API/OpenWeather/index.js
+++ API/OpenWeather/index.js
@@ -1,7 +1,11 @@
 const time = document.getElementById('time')
-const location = document.getElementById('location')
+const loc = document.getElementById('location')
 const weather = document.getElementById('weather')
 
-fetch('https://apis.scrimba.com/openweathermap/data/2.5/weather')
+fetch('https://apis.scrimba.com/openweathermap/data/2.5/weather?q=mures')
     .then(res => res.json())
-    .them(data => console.log(data))
+    .then(data => {
+        time.textContent = new Date()
+        loc.textContent = data.name
+        weather.textContent = `Temperature: ${(data.main.temp - 272.15).toFixed(2)}°C, feels like ${(data.main.feels_like - 272.15).toFixed(2)}°C, air pressure ${data.main.pressure} milibars`
+    })
