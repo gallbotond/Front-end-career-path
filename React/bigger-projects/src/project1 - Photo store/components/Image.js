28bc465d7616a01944c31100af096d42b35069c8
Gáll Botond <gallbotondofficial@gmail.com>
2022-07-11 21:48:45 +0300

    set image hover state

--- React/bigger-projects/src/project1 - Photo store/components/Image.js
+++ React/bigger-projects/src/project1 - Photo store/components/Image.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Image({className, img}) {
+
+  const [hovered, setHovered] = useState(false)
+
+  console.log(hovered)
+
   return (
-    <div className={`image-container ${className}`}>
+    <div className={`image-container ${className}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
       <img src={img.url} className='image-grid' />
     </div>
   )
