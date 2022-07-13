7dcfeb6916df2c0a5368fb64ed1aac1a57190b6e
Gáll Botond <gallbotondofficial@gmail.com>
2022-07-14 00:20:49 +0300

    added noce hover effect

--- React/bigger-projects/src/project1 - Photo store/components/Image.js
+++ React/bigger-projects/src/project1 - Photo store/components/Image.js
@@ -2,8 +2,8 @@ import React, { useContext, useState } from "react";
 import { CustomContext } from "../context/customContext";
 
 export default function Image({ className, img }) {
-  const [hovered, setHovered] = useState(false)
-  const {toggleFavorite} = useContext(CustomContext)
+  const [hovered, setHovered] = useState(false);
+  const { toggleFavorite } = useContext(CustomContext);
 
   return (
     <div
@@ -13,13 +13,13 @@ export default function Image({ className, img }) {
     >
       <img src={img.url} className="image-grid" />
       <div className="inside-image">
+        {hovered && <i className="ri-add-circle-line cart"></i>}
         {hovered && (
           <i
             className={`ri-heart-${img.isFavorite ? "fill" : "line"} favorite`}
             onClick={() => toggleFavorite(img.id)}
           ></i>
         )}
-        {hovered && <i className="ri-add-circle-line cart"></i>}
       </div>
     </div>
   );
